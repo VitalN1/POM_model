14ded6a07f8327ce337be96ecdbd4d6f7ed19ef8
VitalN1 <vital_8marta@mail.ru>
2022-06-15 21:53:05 +0300

    add restore old link

--- test_main_page.py
+++ test_main_page.py
@@ -2,8 +2,8 @@ from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    #link = "http://selenium1py.pythonanywhere.com/"
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+    link = "http://selenium1py.pythonanywhere.com/"
+    #link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
 
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
@@ -15,8 +15,8 @@ def test_guest_can_go_to_login_page(browser):
     #login_link.click()
     # #login_link
 def test_guest_should_see_login_link(browser):
-    # link = "http://selenium1py.pythonanywhere.com/"
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+    link = "http://selenium1py.pythonanywhere.com/"
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
